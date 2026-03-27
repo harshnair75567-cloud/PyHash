8f6862511780cb9a2a34d69181b15e0f931ea3c9
harshnair75567-cloud <harshnair75567@gmail.com>
2026-03-27 20:43:57 +0530

    Simulate attack by modifying config.txt

---
+++ attack_tester.py
@@ -0,0 +1,4 @@
+with open('secure_vault/config.txt', 'w') as f:
+    f.write('System Status: COMPROMISED - Unauthorized Access detected.')
+
+print("--- ATTACK SIMULATED: config.txt has been modified ---")
