3b01cdae5c171c53da421434192cfb0594c5d7e1
harshnair75567-cloud <harshnair75567@gmail.com>
2026-05-01 08:58:54 +0530

    Delete attack_tester.py

--- attack_tester.py
+++
@@ -1,4 +0,0 @@
-with open('secure_vault/config.txt', 'w') as f:
-    f.write('System Status: COMPROMISED - Unauthorized Access detected.')
-
-print("--- ATTACK SIMULATED: config.txt has been modified ---")
